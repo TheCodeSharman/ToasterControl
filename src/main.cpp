f5ed9653f6e34419e6df339c47bdbfe25e65e323
Michael Sharman <michael@thecodesharman.com.au>
2020-09-01 22:37:15 +1000

    ambient temperature reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include <Arduino.h>
 #include "stm32yyxx_ll_adc.h"
 
-#define CALX_TEMP 25
 #if defined(STM32F4xx)
-  //#define V25       760
-  //#define AVG_SLOPE 2500
   #define VREFINT   1210
   #define ADC_RANGE 1024
 #else
@@ -20,10 +17,7 @@ static int32_t readVref()
 
 static int32_t readTempSensor(int32_t VRef)
 {
-  int32_t atemp = analogRead(ATEMP);
-  Serial.printf("ATEMP = %i\n",atemp);
   return (__LL_ADC_CALC_TEMPERATURE(VRef, analogRead(ATEMP), LL_ADC_RESOLUTION_10B));
-  //return (__LL_ADC_CALC_TEMPERATURE_TYP_PARAMS(AVG_SLOPE, V25, CALX_TEMP, VRef, atemp, LL_ADC_RESOLUTION_10B));
 }
 
 void setup() {
@@ -34,8 +28,8 @@ void setup() {
 
 void loop() {
   int32_t vref = readVref();
-  Serial.printf("Vref = %i\n", vref);
+  Serial.printf("Vref = %i", vref);
   int32_t temperature = readTempSensor(vref);
-  Serial.printf("Temp(°C) = %i\n",temperature);
+  Serial.printf("\tTemp(°C) = %i\n",temperature);
   delay(1000);              
 }
