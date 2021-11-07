3cb5aa2796332bbceda9b909ee2fb451ed48a874
Michael Sharman <michael@thecodesharman.com.au>
2021-11-07 14:55:59 +1100

    interpolate from two measured values to calculate temperature

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,13 @@
 // The Vref+ voltage supplied to the STM32 chip.
 const int32_t ADC_VREF = 3300; 
 
-// DC offset of ref pin on the instrument amplifier (units=mV).
-const int32_t GND_REF = 1650;
-const int32_t CAL1 = 19; // calibrate for voltage offset
-const int32_t CAL2 = 3200; // offset from reported ambient temperature
+// measured temperature difference from ambient
+const int32_t CAL_TEMP_OFFSET_A = 26-29;
+const int32_t CAL_TEMP_OFFSET_B = 134-30;
 
-// IN126 gain resister value (units=ohms).
-const int32_t GAIN_RESISTER = 645; 
-
-// Formula for gain from the IN126 datasheet.
-const int32_t GAIN = 125; //5 + 80000/GAIN_RESISTER;
+// corresponding measured ADC values
+const int32_t CAL_ADC_A = 2022;
+const int32_t CAL_ADC_B = 2716;
 
 static int32_t readAverage(uint32_t pin) {
   int32_t value = 0;
@@ -45,28 +42,18 @@ static int32_t readInternalTempSensor()
 }
 
 static int32_t readKTypeProbeSensor() {
-  /* 1. Calculate the measured voltage on the ADC and subtract the ref voltage
-        from this value to get the amplified voltage from the probe (units=mV). */
-  int32_t k_probe_amplified = __LL_ADC_CALC_DATA_TO_VOLTAGE(ADC_VREF,
-    readAverage(K_TYPE_PROBE),LL_ADC_RESOLUTION_12B) - GND_REF + CAL1;
 
-  Serial.printf("\nk_probe_amplified = %i\n", k_probe_amplified);
-
-  /* 2. Convert this value into µV value measured at the K type probe (before 
-        amplification). */
-  int32_t k_probe_uv = k_probe_amplified*1000/GAIN; 
-
-  Serial.printf("k_probe_uv = %i\n", k_probe_uv);
-
-  /* 3. Using the linear polygon fit to the NIST K type probe data calculate
-        the temperature delta from the cold junction to the hot junction. */
-  int32_t k_probe_temp_delta = (k_probe_uv*24487)/1000 + 312 - CAL2;
-
-  /* 4. Using the internal temperature sensor, convert to an
-        absolute temperature. */
   int32_t die_temp = readInternalTempSensor();
-  Serial.printf("die_temp = %i\n", die_temp);
-  return k_probe_temp_delta/1000 + die_temp;
+  int32_t probe_val = readAverage(K_TYPE_PROBE);
+
+  // Interpolate value based on calibration constants
+  int32_t k_probe_temp_delta 
+    = CAL_TEMP_OFFSET_A + 
+      (CAL_TEMP_OFFSET_B - CAL_TEMP_OFFSET_A) *
+      (probe_val-CAL_ADC_A)/(CAL_ADC_B-CAL_ADC_A);
+  
+  Serial.printf("die_temp = %i probe_val = %i k_probe_temp_delta = %i\n", die_temp, probe_val, k_probe_temp_delta);
+  return k_probe_temp_delta + die_temp;
 }
 
 void setup() {
