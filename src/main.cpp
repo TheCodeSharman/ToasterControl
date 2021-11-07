2ad9f36d6a2813d4f7a1b608276cec55786699e9
Michael Sharman <michael@thecodesharman.com.au>
2021-11-07 14:11:11 +1100

    physics based calcs tweaked

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,16 @@ const int32_t CAL2 = 3200; // offset from reported ambient temperature
 const int32_t GAIN_RESISTER = 645; 
 
 // Formula for gain from the IN126 datasheet.
-const int32_t GAIN = 5 + 80000/GAIN_RESISTER;
+const int32_t GAIN = 125; //5 + 80000/GAIN_RESISTER;
+
+static int32_t readAverage(uint32_t pin) {
+  int32_t value = 0;
+  int32_t i;
+  for(i=0; i<100; i++ ) {
+    value += analogRead(pin);
+  }
+  return value/i;
+}
 
 /*
   In order to make the circuit simpler, we read the STM32 chip temperature sensor
@@ -26,23 +35,28 @@ const int32_t GAIN = 5 + 80000/GAIN_RESISTER;
   For this to work we are assuming that there is a constant thermal resistance between 
   the microproccesor and the cold end of the K type probe. 
 
-  Certainly not a precision measurement, but it seems accurate to a couple of degrees,
-  so saves us measuring the cold junction with a thermister.
+  Certainly not a precision measurement, but saves us measuring the cold junction with 
+  a thermister. The hope is that when the board is enclosed this offset will be
+  sufficiently stable.
 */
 static int32_t readInternalTempSensor()
 {
-  return __LL_ADC_CALC_TEMPERATURE(ADC_VREF, analogRead(ATEMP), LL_ADC_RESOLUTION_12B);
+  return __LL_ADC_CALC_TEMPERATURE(ADC_VREF, readAverage(ATEMP), LL_ADC_RESOLUTION_12B);
 }
 
 static int32_t readKTypeProbeSensor() {
   /* 1. Calculate the measured voltage on the ADC and subtract the ref voltage
         from this value to get the amplified voltage from the probe (units=mV). */
   int32_t k_probe_amplified = __LL_ADC_CALC_DATA_TO_VOLTAGE(ADC_VREF,
-    analogRead(K_TYPE_PROBE),LL_ADC_RESOLUTION_12B) - GND_REF + CAL1;
+    readAverage(K_TYPE_PROBE),LL_ADC_RESOLUTION_12B) - GND_REF + CAL1;
+
+  Serial.printf("\nk_probe_amplified = %i\n", k_probe_amplified);
 
   /* 2. Convert this value into µV value measured at the K type probe (before 
         amplification). */
   int32_t k_probe_uv = k_probe_amplified*1000/GAIN; 
+
+  Serial.printf("k_probe_uv = %i\n", k_probe_uv);
 
   /* 3. Using the linear polygon fit to the NIST K type probe data calculate
         the temperature delta from the cold junction to the hot junction. */
@@ -51,6 +65,7 @@ static int32_t readKTypeProbeSensor() {
   /* 4. Using the internal temperature sensor, convert to an
         absolute temperature. */
   int32_t die_temp = readInternalTempSensor();
+  Serial.printf("die_temp = %i\n", die_temp);
   return k_probe_temp_delta/1000 + die_temp;
 }
 
