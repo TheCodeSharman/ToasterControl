2de58c75d44f230b01a38aafdf0675363b4dec26
Michael Sharman <michael@thecodesharman.com.au>
2021-11-09 20:57:18 +1100

    rename function to better imply intent

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ const uint8_t LED = PC13;
 KTypeProbe probe(A2,3300,-3,104,2022,2716);
 Every executor;
 
-void updateAndDisplayProbeStats() {
+void displayProbeStats() {
   Serial.printf("Probe Temp(°C) = %i\n", probe.getTemperature()/1000);
 }
 
@@ -22,7 +22,7 @@ void setup() {
   pinMode(LED, OUTPUT);  
   digitalWrite(LED, LOW);
   analogReadResolution(12);
-  executor.every(1000,updateAndDisplayProbeStats);
+  executor.every(1000,displayProbeStats);
   executor.every(500,blinkLed);
 }
 
