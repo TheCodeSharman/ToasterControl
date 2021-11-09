94713e0896651f5dcccf2fe597fb44bd31fc5bbc
Michael Sharman <michael@thecodesharman.com.au>
2021-11-09 21:18:14 +1100

    minor typo fixed

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ const uint8_t LED = PC13;
 KTypeProbe probe(A2,3300,-3,104,2022,2716);
 MultiTask tasks;
 
-void displayProbeStats() {
+void displayProbeStatus() {
   Serial.printf("Probe Temp(°C) = %i (coldJunction = %i, probeAdc = %u)\n", 
     probe.getTemperature()/1000, probe.getColdJunction()/1000, probe.getProbeAdc() );
 }
@@ -23,7 +23,7 @@ void setup() {
   pinMode(LED, OUTPUT);  
   digitalWrite(LED, LOW);
   analogReadResolution(12);
-  tasks.every(1000,displayProbeStats);
+  tasks.every(1000,displayProbeStatus);
   tasks.every(500,blinkLed);
 }
 
