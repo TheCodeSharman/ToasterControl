97a368072912d2b57df6730f00df1df60666bdb7
Michael Sharman <michael@thecodesharman.com.au>
2021-11-09 20:56:38 +1100

    decoupling status output from sensor reading

---
+++ src/Every.h
@@ -0,0 +1,57 @@
+#ifndef EVERY_H
+#define EVERY_H
+
+#include <Arduino.h>
+#include <vector>
+
+/*
+    Keeps track of multiple functions to be called at specified intervals.
+
+    To use declare an instance of Every:
+
+            Every executor;
+
+        In some initialisation function add some callbacks:
+
+            executor.every(1000,callbackFunction1);
+            executor.every(500,callbackFunction2);
+
+        Then in the main loop call process():
+
+            executor.process();
+
+    It's up to the impementor to ensure there is no blocking code and return 
+    promptly - otherwise nothing will be called whilst a function is being run.
+*/
+class Every {
+  uint32_t current;
+  typedef struct  {
+    void (*callback)();
+    uint32_t nextCall;
+    uint32_t period;
+  } CallbackFunction;
+
+  std::vector<CallbackFunction> callbacks;
+
+  public:
+    /* Add a call back function to be called every time the clock ticks over the 
+       specified delay in milliseconds */
+    void every(uint32_t delay, void (*callback)()) {
+      CallbackFunction cb = { callback, 0, delay };
+      callbacks.push_back(cb);
+    }
+
+    /* Needs to be called as often as possible */
+    void process() {
+      current = millis();
+      for(auto &cb : callbacks ) {
+        if ( cb.nextCall < current ) {
+          cb.nextCall = current+cb.period;
+          cb.callback();
+        }
+      }
+    }
+};
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,32 @@
 #include <Arduino.h>
+
+#include "Every.h"
 #include "KTypeProbe.h"
 
-#define LED PC13
+const uint8_t LED = PC13;
 
 KTypeProbe probe(A2,3300,-3,104,2022,2716);
+Every executor;
+
+void updateAndDisplayProbeStats() {
+  Serial.printf("Probe Temp(°C) = %i\n", probe.getTemperature()/1000);
+}
+
+void blinkLed() {
+  static bool isLedOn = false;
+  digitalWrite(LED, (isLedOn?HIGH:LOW));
+  isLedOn = !isLedOn;
+}
 
 void setup() {
   pinMode(LED, OUTPUT);  
   digitalWrite(LED, LOW);
   analogReadResolution(12);
+  executor.every(1000,updateAndDisplayProbeStats);
+  executor.every(500,blinkLed);
 }
 
 void loop() {
-  digitalWrite(LED, LOW);
-  probe.update();
-  Serial.printf("Probe Temp(°C) = %i\n", probe.getTemperature()/1000);
-
-  delay(500);   
-  digitalWrite(LED, HIGH);  
-  delay(500);         
+  executor.process();   
+  probe.update();      
 }
